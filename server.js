4cc8468e109cbfd7c6d54fc67c25f59a80768973
RaphaelMa <raphaelmariano197@gmail.com>
2025-06-07 23:33:44 -0300

    fix: adjusted message fot hello world argo

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
-app.get('', (req, res) => {
-    res.send('<h1>Hello world</h1>');
-})
-
+app.get("", (req, res) => {
+  res.send("<h1>Hello world Argo</h1>");
+});
 
 app.listen(3000, () => {
-    console.log(`Server is running on port ${PORT}`) 
+  console.log(`Server is running on port ${PORT}`);
 });
